b3d4cb541baa1cf0455e2f210a9dee04db29ec33
Daniel von Eßen <daniel.von-essen@deutschebahn.com>
2020-10-20 06:41:41 +0000

    f

--- gcip/__init__.py
+++ gcip/__init__.py
@@ -7,6 +7,7 @@ from ._core.job import Job  # noqa
 from ._core.core import Core  # noqa
 from ._core.rule import Rule  # noqa
 from ._core.include import Include, IncludeMethod  # noqa
+from ._core.trigger import Trigger, TriggerStrategy  # noqa
 from ._core.pipeline import Pipeline  # noqa
 from ._core.job_sequence import JobSequence  # noqa
 
