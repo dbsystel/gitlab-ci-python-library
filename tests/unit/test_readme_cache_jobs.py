a5b115995262d6c9bb50cc22830f6bdddccb5841
Daniel von Eßen <daniel.von-essen@deutschebahn.com>
2021-02-22 08:39:34 +0100

    Added pipeline test to test cache.

---
+++ tests/unit/test_readme_cache_jobs.py
@@ -0,0 +1,14 @@
+import gcip
+from tests import conftest
+
+
+def test():
+    pipeline = gcip.Pipeline()
+    cache = gcip.Cache(["dir1", "dir2", "dir3/subdir"])
+    cachejob1 = gcip.Job(name="cachejob1", namespace="single-stage", script="date")
+    cachejob2 = gcip.Job(name="cachejob2", namespace="single-stage", script="date")
+    cachejob1.set_cache(cache)
+    cachejob2.set_cache(cache)
+    pipeline.add_jobs(cachejob1, cachejob2)
+
+    conftest.check(pipeline.render())
