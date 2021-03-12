8756fe8b85bbd84711edeaf05520a31e58c3d2b5
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-03-12 21:54:37 +0100

    the docker image now installs requirements.txt files located in the project dir

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+set -x
+
 PIPELINE_PY="${*:-.gitlab-ci.py}"
+
+if test -f requirements.txt; then
+  grep -v '^ *#\|^gcip' requirements.txt > /tmp/gcip_requirements.tmp
+  cat /tmp/gcip_requirements.tmp
+  pip3 install -r /tmp/gcip_requirements.tmp
+  rm -rf /tmp/gcip_requirements.tmp
+fi
+
 python3 "$PIPELINE_PY"
