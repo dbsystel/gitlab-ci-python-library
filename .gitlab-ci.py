703be804a9865b486a474dab66373c7e1863d351
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-03-13 10:41:07 +0100

    build gcip docker image with proper tagging

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -9,6 +9,7 @@ pipeline.add_children(
     python.isort(), python.flake8(), python.pytest(), python.evaluate_git_tag_pep404_conformity(),
     kaniko.execute(
         image_name="thomass/gcip",
+        image_tag=(PredefinedVariables.CI_COMMIT_TAG[1:] if PredefinedVariables.CI_COMMIT_TAG else PredefinedVariables.CI_COMMIT_REF_SLUG),
         enable_push=(PredefinedVariables.CI_COMMIT_TAG or PredefinedVariables.CI_COMMIT_BRANCH == "main"),
         dockerhub_user_env_var="DOCKER_USER",
         dockerhub_login_env_var="DOCKER_LOGIN",
