740035b43e84cf608dcd9e559e8e694dbd1fb69a
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-03-15 12:53:21 +0100

    changed signature of gcip.addons.python.jobs.twine_upload() to be more secure and self explaining

--- gcip/addons/python/jobs.py
+++ gcip/addons/python/jobs.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from gcip.lib import rules
 from gcip.core.job import Job
 
@@ -145,9 +147,9 @@ def pages_sphinx() -> Job:
 
 
 def twine_upload(
-    repository_url: str,
-    user: str,
-    varname_password: str,
+    twine_repository_url: str,
+    twine_username_env_var: Optional[str] = "TWINE_USERNAME",
+    twine_password_env_var: Optional[str] = "TWINE_PASSWORD",
 ) -> Job:
     """
     Runs:
@@ -159,15 +161,14 @@ def twine_upload(
 
     * Requires artifacts from a build job under `dist/` (e.g. from `bdist_wheel()`)
 
-    :arg repository_url: The URL to the PyPI repository the python artifacts will be deployed to.
-    :arg user: The name of the user to access the PyPI repository.
-    :arg varname_password: The name of the environment variable delivering the password to access the PyPI repository.
-    If not existent, automatically a "$" will be prepended to the string. DO NOT DEFINE THE PASSWORD WITHIN THE PIPELINE.
-    Define your password outside the pipeline, e.g. as secret variable in the Gitlab CI/CD settings section.
+    Args:
+        twine_repository_url (str): The URL to the PyPI repository the python artifacts will be deployed to. Defaults
+            to `None`, which means the package is published to `https://pypi.org`.
+        twine_username_env_var (Optional[str]): The name of the environment variable, which contains the username value.
+            **DO NOT PROVIDE THE USERNAME VALUE ITSELF!** This would be a security issue! Defaults to `TWINE_USERNAME`.
+        twine_password_env_var (Optional[str]): The name of the environment variable, which contains the password.
+            **DO NOT PROVIDE THE LOGIN VALUE ITSELF!** This would be a security issue! Defaults to `TWINE_PASSWORD`.
     """
-    if not varname_password.startswith("$"):
-        varname_password = "$" + varname_password
-
     job = Job(
         name="twine_upload",
         namespace="deploy",
@@ -177,8 +178,11 @@ def twine_upload(
         ],
     )
     job.add_variables(
-        TWINE_REPOSITORY_URL=repository_url,
-        TWINE_USERNAME=user,
-        TWINE_PASSWORD=varname_password,
+        TWINE_USERNAME=f"${twine_username_env_var}",
+        TWINE_PASSWORD=f"${twine_password_env_var}",
     )
+
+    if twine_repository_url:
+        job.add_variables(TWINE_REPOSITORY_URL=twine_repository_url)
+
     return job
