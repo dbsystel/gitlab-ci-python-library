2e80cbb202b1c1b18fcfd84e6f3ea0402b3c302a
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-03-15 17:25:13 +0100

    updated pytests according to changes of the gcip.addons.python.jobs.twine_upload() interface

--- tests/unit/test_python_full_stack.py
+++ tests/unit/test_python_full_stack.py
@@ -9,11 +9,11 @@ def test():
     pipeline.add_children(
         python.full_stack(
             dev_repository_url="https://my.artifactory.net/pypi/dev-repository",
-            dev_user="$ARTIFACTORY_DEV_USER",
-            varname_dev_password="$ARTIFACTORY_DEV_PASSWORD",
+            dev_user="ARTIFACTORY_DEV_USER",
+            varname_dev_password="ARTIFACTORY_DEV_PASSWORD",
             stable_repository_url="https://my.artifactory.net/pypi/prod-repository",
-            stable_user="$ARTIFACTORY_PROD_USER",
-            varname_stable_password="$ARTIFACTORY_PROD_PASSWORD",
+            stable_user="ARTIFACTORY_PROD_USER",
+            varname_stable_password="ARTIFACTORY_PROD_PASSWORD",
         )
     )
 
@@ -25,11 +25,11 @@ def test_with_mypy():
     pipeline.add_children(
         python.full_stack(
             dev_repository_url="https://my.artifactory.net/pypi/dev-repository",
-            dev_user="$ARTIFACTORY_DEV_USER",
-            varname_dev_password="$ARTIFACTORY_DEV_PASSWORD",
+            dev_user="ARTIFACTORY_DEV_USER",
+            varname_dev_password="ARTIFACTORY_DEV_PASSWORD",
             stable_repository_url="https://my.artifactory.net/pypi/prod-repository",
-            stable_user="$ARTIFACTORY_PROD_USER",
-            varname_stable_password="$ARTIFACTORY_PROD_PASSWORD",
+            stable_user="ARTIFACTORY_PROD_USER",
+            varname_stable_password="ARTIFACTORY_PROD_PASSWORD",
             mypy_package_dir="test_package_dir",
         )
     )
