27fbc5fe58787d22b9fb654bd2cfebdf22cf7a99
Daniel von Eßen <daniel.von-essen@deutschebahn.com>
2021-03-18 13:16:22 +0000

    Added new `class` which handels docker client config and renders it to a json string.

---
+++ tests/unit/test_addons_container_config.py
@@ -0,0 +1,21 @@
+from tests import conftest
+from gcip.addons.container.config import DockerClientConfig
+
+
+def test_docker_client_config():
+    dcc = DockerClientConfig()
+    dcc.add_auths("index.docker.com")
+    conftest.check(dcc.render())
+
+
+def test_complex_docker_client_config():
+    dcc = DockerClientConfig()
+    dcc.add_auths("index.docker.com", "CUSTOM_DOCKER_USER", "CUSTOM_DOCKER_PW")
+    dcc.add_cred_helper("123456789.dkr.ecr.eu-central-1.amazonaws.com", "ecr-login")
+    dcc.set_creds_store("gcr")
+    dcc.add_raw({"proxies": {
+        "default": {
+            "httpProxy": "127.0.0.1:1234"
+        }
+    }})
+    conftest.check(dcc.render())
