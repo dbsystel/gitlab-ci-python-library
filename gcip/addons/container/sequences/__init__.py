41b7303e7fcb0e90442a898ed9dbe745b2fb6706
Daniel von Eßen <daniel.von-essen@deutschebahn.com>
2021-04-13 06:55:31 +0000

    Added new container sequence. Container sequence build, scans and pushes an container image.

---
+++ gcip/addons/container/sequences/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Thomas Steinbach"
+__copyright__ = "Copyright 2020 DB Systel GmbH"
+__credits__ = ["Thomas Steinbach", "Daniel von Eßen"]
+# SPDX-License-Identifier: Apache-2.0
+__license__ = "Apache-2.0"
+__maintainer__ = "Daniel von Eßen"
+__email__ = "daniel.von-essen@deutschebahn.com"
