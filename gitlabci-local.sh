a94634f1245247f5df98cd4d72eade7e7f9ffbcb
Daniel von Eßen <daniel.von-essen@deutschebahn.com>
2021-04-14 09:26:43 +0000

    Changed deployment pipeline, using build.build_scan_push_image. Changed base image from Debian base to Alpine. Simplyfied gitlabci-local and added REGISTRY_* exports to it.

--- gitlabci-local.sh
+++ gitlabci-local.sh
@@ -10,7 +10,23 @@ then
 fi
 
 echo "Powerd by https://pypi.org/project/gitlabci-local/"
-gitlabci-local -p -e CI="true" -e CI_PROJECT_DIR="/builds/gitlab-ci-python-library" -e CI_COMMIT_REF_SLUG="gitlab-local-sh" && cat generated-config.yml && gitlabci-local -c generated-config.yml $@
+
+if [ -z $REGISTRY_USERNAME ]; then
+    read -p "Enter container registry user: " REGISTRY_USERNAME
+fi
+if [ -z $REGISTRY_PASSWORD ]; then
+    read -sp "Enter container registry password: " REGISTRY_PASSWORD
+fi
+gitlabci_local_envs="-e CI=true
+    -e CI_PROJECT_NAME=gitlab-ci-python-library
+    -e CI_PROJECT_DIR=/builds/gitlab-ci-python-library
+    -e CI_COMMIT_REF_SLUG=gitlab-local-sh
+    -e REGISTRY_USERNAME=${REGISTRY_USERNAME}
+    -e REGISTRY_PASSWORD=${REGISTRY_PASSWORD}"
+
+gitlabci-local -p $gitlabci_local_envs && \
+cat generated-config.yml && \
+gitlabci-local $gitlabci_local_envs -c generated-config.yml $@
 
 rm -rf generated-config.yml
 rm -rf dist
