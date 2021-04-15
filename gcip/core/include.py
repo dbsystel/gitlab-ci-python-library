3291414de1f6383e91dea6bb222a5a8a37b75b1b
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-04-15 23:48:38 +0200

    Added full API documentation of the  module.

--- gcip/core/include.py
+++ gcip/core/include.py
@@ -1,3 +1,55 @@
+"""This module represents the Gitlab CI [Include](https://docs.gitlab.com/ee/ci/yaml/#include) keyword.
+
+Use include to include external YAML files in your CI/CD configuration.
+
+----
+
+[include:local](https://docs.gitlab.com/ee/ci/yaml/#includelocal) example:
+
+```
+pipeline.add_include(IncludeLocal("/templates/.gitlab-ci-template.yml"))
+```
+
+----
+
+[include:file](https://docs.gitlab.com/ee/ci/yaml/#includefile) example:
+
+```
+pipeline.add_include(IncludeFile(
+        project="my-group/my-project",
+        ref="master",
+        file="/templates/.gitlab-ci-template.yml"
+    ))
+```
+
+----
+
+[include:remote](https://docs.gitlab.com/ee/ci/yaml/#includeremote) example:
+
+```
+pipeline.add_include(IncludeRemote("https://gitlab.com/example-project/-/raw/master/.gitlab-ci.yml"))
+```
+
+----
+
+[include:template](https://docs.gitlab.com/ee/ci/yaml/#includetemplate) example:
+
+```
+pipeline.add_include(IncludeTemplate("Auto-DevOps.gitlab-ci.yml"))
+```
+
+----
+
+Special type of include: Use a `gcip.core.job.TriggerJob` with `IncludeArtifact` to run [a child pipeline with a generated
+configuration file from a previous job](https://docs.gitlab.com/ee/ci/yaml/README.html#trigger-child-pipeline-with-generated-configuration-file):
+
+```
+TriggerJob(includes=IncludeArtifact(job="generate-config", artifact="generated-config.yml"))
+```
+
+Note: The `IncludeArtifact` isn't implemented very well as it currently cannot handle `gcip.core.job.Job` objects. You need to know the jobs final name,
+which is not very handy. This could be implemented much better in future.
+"""
 from abc import ABCMeta
 from typing import Dict, Optional
 
@@ -13,19 +65,52 @@ __email__ = "thomas.t.steinbach@deutschebahn.com"
 
 
 class Include(metaclass=ABCMeta):
+    """
+    This is just an abstract superclass.
+
+    Please use one of the subclasses:
+
+    * `IncludeLocal`
+    * `IncludeFile`
+    * `IncludeRemote`
+    * `IncludeTemplate`
+    * `IncludeArtifact`
+    """
 
     _rendered_include: Dict[str, str]
 
     def render(self) -> Dict[str, str]:
+        """Returns a representation of this Include object as dictionary with static values.
+
+        The rendered representation is used by the gcip to dump it
+        in YAML format as part of the .gitlab-ci.yml pipeline.
+
+        Returns:
+            Dict[str, Any]: A dictionary representing the include object in Gitlab CI.
+        """
         return self._rendered_include
 
 
 class IncludeLocal(Include):
+    """This module represents the Gitlab CI [include:local](https://docs.gitlab.com/ee/ci/yaml/#includelocal) keyword.
+
+    Args:
+        local (str): The path to the file within this repository to include.
+    """
+
     def __init__(self, local: str) -> None:
         self._rendered_include = {"local": local}
 
 
 class IncludeFile(Include):
+    """This module represents the Gitlab CI [include:file](https://docs.gitlab.com/ee/ci/yaml/#includefile) keyword.
+
+    Args:
+        file (str): The path to the file to include.
+        project (str): The project to include the file from.
+        ref (Optional[str], optional): The branch of the project to include the file from. Defaults to None.
+    """
+
     def __init__(
         self,
         file: str,
@@ -38,6 +123,15 @@ class IncludeFile(Include):
 
 
 class IncludeRemote(Include):
+    """This module represents the Gitlab CI [include:remote](https://docs.gitlab.com/ee/ci/yaml/#includeremote) keyword.
+
+    Args:
+        remote (str): The URL to include the a file from.
+
+    Raises:
+        ValueError: When the `remote` is not a valid URL.
+    """
+
     def __init__(self, remote: str) -> None:
         if not is_valid_url(remote):
             raise ValueError(f"`remote` is not a valid URL: {remote}")
@@ -46,12 +140,24 @@ class IncludeRemote(Include):
 
 
 class IncludeTemplate(Include):
+    """This module represents the Gitlab CI [include:template](https://docs.gitlab.com/ee/ci/yaml/#includetemplate) keyword.
+
+    Args:
+        template (str): The Gitlab template pipeline to include.
+    """
+
     def __init__(self, template: str):
         self._rendered_include = {"template": template}
 
 
 class IncludeArtifact(Include):
-    """for triggering a child pipeline with generated configuration file"""
+    """A special type of include: Use a `gcip.core.job.TriggerJob` with `IncludeArtifact` to run [a child pipeline with a generated configuration
+    file from a previous job](https://docs.gitlab.com/ee/ci/yaml/README.html#trigger-child-pipeline-with-generated-configuration-file):
+
+    Args:
+        job (str): The job name to include the artifact from.
+        artifact (str): The name of the job artifact to include.
+    """
 
     def __init__(self, job: str, artifact: str):
         self._rendered_include = {"job": job, "artifact": artifact}
