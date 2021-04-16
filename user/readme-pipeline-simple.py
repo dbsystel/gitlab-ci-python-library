b2ae91f18cbbd1dedead1d7a8fe51ecd14c77635
dvonessen <dvonessen@users.noreply.github.com>
2021-04-16 09:32:32 +0000

    Deploying to gh-pages from @ dbsystel/gitlab-ci-python-library@139bfd0a6933ac38939d9fe10c3edf8ff9d70f59 🚀

--- user/readme-pipeline-simple.py
+++ user/readme-pipeline-simple.py
@@ -1,5 +1,5 @@
 from gcip import Job, Pipeline
 
 pipeline = Pipeline()
-pipeline.add_children(Job(stage="build", script="docker build ."))
+pipeline.add_children(Job(namespace="build", script="docker build ."))
 pipeline.write_yaml()
