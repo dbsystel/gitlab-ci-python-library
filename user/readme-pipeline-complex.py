e5ee76b3d78e009853dc5793f7df61e3c130df55
ThomasSteinbach <ThomasSteinbach@users.noreply.github.com>
2021-04-16 10:03:42 +0000

    Deploying to gh-pages from @ dbsystel/gitlab-ci-python-library@704bda946c8418c5d04159fe975018d8abbb89ce 🚀

--- user/readme-pipeline-complex.py
+++ user/readme-pipeline-complex.py
@@ -4,8 +4,8 @@ from gcip.addons.gitlab import job_scripts as gitlab
 
 def get_build_deploy_sequence(environment: str):
     return JobSequence().add_children(
-        Job(namespace="build", script=f"docker build -t myimage-{environment} ."),
-        Job(namespace="deploy", script=["docker login", f"docker push myimage-{environment}"]),
+        Job(stage="build", script=f"docker build -t myimage-{environment} ."),
+        Job(stage="deploy", script=["docker login", f"docker push myimage-{environment}"]),
     )
 
 
@@ -20,6 +20,6 @@ for environment in ("develop", "test", "production"):
     )
     jobs.add_tags(environment)
 
-    pipeline.add_children(jobs, namespace=environment)
+    pipeline.add_children(jobs, stage=environment)
 
 pipeline.write_yaml("generated-config.yml")
