af6dbbd5297a6036216dc359a3cf88ceade3802b
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-04-19 23:26:47 +0200

    added documentation to the service module

--- gcip/core/service.py
+++ gcip/core/service.py
@@ -1,8 +1,29 @@
+"""**ALPHA** This module represents the Gitlab CI [Service](https://docs.gitlab.com/ee/ci/yaml/README.html#services) keyword.
+
+The services keyword defines a Docker image that runs during a job linked to the Docker image that the image keyword defines.
+This allows you to access the service image during build time.
+
+Currently this module is an unfinished prototype.
+"""
+
+
 class Service:
-    """This class represents the Gitlab CI [Service](https://docs.gitlab.com/ee/ci/yaml/README.html#services) keyword."""
+    """**ALPHA** This class represents the Gitlab CI [Service](https://docs.gitlab.com/ee/ci/yaml/README.html#services) keyword.
+
+    Currently there is nothing more implemented than providing a service name. In general the `service` functionality
+    currently isn't well implemented, as is is only available for `gcip.core.pipeline.Pipeline`s.
+    """
 
     def __init__(self, name: str):
         self._name = name
 
     def render(self) -> str:
+        """Return a representation of this Service object as dictionary with static values.
+
+        The rendered representation is used by the gcip to dump it
+        in YAML format as part of the .gitlab-ci.yml pipeline.
+
+        Returns:
+            Dict[str, Any]: A dictionary representing the service object in Gitlab CI.
+        """
         return self._name
