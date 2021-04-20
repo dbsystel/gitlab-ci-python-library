e6a58bdae1ccd03bd21a034f49c2bcb67a7a3360
Thomas Steinbach <thomas.t.steinbach@deutschebahn.com>
2021-04-20 22:15:01 +0200

    moved to gitlab.com

--- docs/user/readme-pipeline-simple.py
+++
@@ -1,5 +0,0 @@
-from gcip import Job, Pipeline
-
-pipeline = Pipeline()
-pipeline.add_children(Job(stage="build", script="docker build ."))
-pipeline.write_yaml()
--- tests/unit/test_readme_pipe_with_one_job.py
+++
@@ -1,9 +0,0 @@
-import gcip
-from tests import conftest
-
-
-def test():
-    pipeline = gcip.Pipeline()
-    pipeline.add_children(gcip.Job(stage="print_date", script="date"))
-
-    conftest.check(pipeline.render())
